d505481aa68847d550b7464b593524e22de836c6
Radek Grebski <radek.grebski@stepwise.pl>
2021-12-06 19:34:36 +0100

    Changed helloWorld to handleRequest

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import type {HttpFunction} from '@google-cloud/functions-framework/build/src/functions';
 
 
-export const helloWorld: HttpFunction = (req, res) => {
+export const handleRequest: HttpFunction = (req, res) => {
     const command = req.body.text
     res.send(`Command: '${command}'`);
 };
